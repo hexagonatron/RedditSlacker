7d9bf760072681ed2d1a38d89415a8ac87446b5d
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-04-25 22:15:43 +0930

    Removing old code

--- index.js
+++ index.js
@@ -241,11 +241,11 @@ app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-    console.log(req.headers.host);
-    logRequest(req);
-    res.status("200").send();
-})
+// app.get("/", (req, res) => {
+//     console.log(req.headers.host);
+//     logRequest(req);
+//     res.status("200").send();
+// })
 
 app.post("/subreddit", jsonParser, (req, res) => {
     res.status("200").send();
@@ -279,63 +279,4 @@ app.post("/subreddit", jsonParser, (req, res) => {
             postToSlack(slackResponse, body.channel_id);
         });
 
-})
-
-// const body = new FormData();
-// body.append("grant_type", "client_credentials");
-
-
-/* fetch("https://www.reddit.com/api/v1/access_token",{
-    method: "POST",
-    headers: {
-        'Authorization': 'Basic ' + btoa(`${REDDIT_APP_ID}:${REDDIT_APP_SECRET}`),
-        "User-Agent": "Script slackbot by hexagonatron",
-    },
-    body: body
-})
-.then(response => {
-    return response.json();
-})
-.then(json => {
-    const access_token = json.access_token;
-
-    return fetch("https://oauth.reddit.com/r/tifu/top",{
-        method:"GET",
-        headers: {
-            "Authorization": "bearer "+ access_token
-        }
-    });
-})
-.then(response => {
-    return response.json();
-})
-.then(subRedditJson => {
-
-    console.log(subRedditJson.data.children[0].data.title);
-    console.log(subRedditJson.data.children[0].data.selftext);
-    const imgURL = subRedditJson.data.children[0].data.url;
-
-    fetch("https://slack.com/api/chat.postMessage", {
-        method: "POST",
-        headers: {
-            "Content-type":"application/json; charset=utf-8",
-            "Authorization": "Bearer "+ SLACK_TOKEN
-        },
-        body:JSON.stringify({
-            channel: "#appdev",
-            text: "Sending from NodeJS =D",
-            blocks: `[{
-                "type": "image",
-                "image_url": "${imgURL}",
-                "alt_text": "An image"
-            }]`
-        })
-    })
-    .then(response=> {
-        return response.json()
-    })
-    .then(json => {
-        console.log(json);
-    })
-
-}) */
+});
