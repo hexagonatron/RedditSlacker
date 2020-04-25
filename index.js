054a52bac67b1b6e1b540f69af1b991d93d4808f
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-04-25 22:14:12 +0930

    Bug fixes

--- index.js
+++ index.js
@@ -87,7 +87,9 @@ const queryReddit = (subreddit, timeFrame = "day") => {
                 return response.json();
             })
             .then(json => {
-                res(json.data.children);
+                console.log(json);
+                if (json.data.children.length) res(json.data.children);
+                else rej("Cannot find any posts in your subreddit for the provided timeframe");
             })
             .catch(err => {
                 rej(err);
@@ -96,7 +98,9 @@ const queryReddit = (subreddit, timeFrame = "day") => {
 }
 
 const createResponse = (post, userID) => {
-    console.log(post);
+    // console.log(post);
+
+    //If it's a text post format response for text
     if (post.is_self){
 
         return JSON.stringify([
@@ -110,9 +114,8 @@ const createResponse = (post, userID) => {
                 {
                     "type": "section",
                     "text": {
-                        "type": "markdwn",
-                        "text": post.selftext,
-                        "emoji": true
+                        "type": "mrkdwn",
+                        "text": post.selftext.slice(0, 3000)
                     }
                 },
                 {
@@ -129,7 +132,41 @@ const createResponse = (post, userID) => {
 
 
     } else {
-        return `[
+        //Gfycat decoder
+        post.url = /(^https:\/\/gfycat\.com\/(.*))/gm.exec(post.url)?post.media.oembed.thumbnail_url:post.url;
+
+        //imgur decoder
+        post.url = /^(https:\/\/i\.imgur\.com\/.*\.gif)v$/g.exec(post.url)?/^(https:\/\/i\.imgur\.com\/.*\.gif)v$/g.exec(post.url)[1]:post.url;
+
+        //if it's an image link format for an image
+        if(/(\.jpg|\.gif|\.png)$/gim.test(post.url)){
+
+            return `[
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "*${post.title}*"
+                        }
+                    },
+                    {
+                        "type": "image",
+                        "image_url": "${post.url}",
+                        "alt_text": "image1"
+                    },
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": "Bot called by <@${userID}>"
+                            }
+                        ]
+                    }
+                ]`
+        } else {
+            //if not an image just return the link
+            return `[
                 {
                     "type": "section",
                     "text": {
@@ -138,9 +175,11 @@ const createResponse = (post, userID) => {
                     }
                 },
                 {
-                    "type": "image",
-                    "image_url": "${post.url}",
-                    "alt_text": "image1"
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "${post.url}"
+                    }
                 },
                 {
                     "type": "context",
@@ -152,7 +191,29 @@ const createResponse = (post, userID) => {
                     ]
                 }
             ]`
+        }
     }
+}
+
+const createError = (error, userID) => {
+    return JSON.stringify([
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": error
+            }
+        },
+        {
+            "type": "context",
+            "elements": [
+                {
+                    "type": "mrkdwn",
+                    "text": `Bot called by <@${userID}>`
+                }
+            ]
+        }
+    ]);
 }
 
 const postToSlack = (block, channel) => {
@@ -197,15 +258,22 @@ app.post("/subreddit", jsonParser, (req, res) => {
     const { body } = req;
 
     const optionsArray = body.text.split(' ');
-    const subreddit = optionsArray[0];
+    const subreddit = optionsArray[0]?optionsArray[0]: "funny" ;
     const timeFrame = optionsArray[1]? optionsArray[1]: "day";
 
     queryReddit(subreddit, timeFrame)
         .then(posts => {
+            console.log(`Count: ${posts.length}`);
             const randPost = posts[Math.floor(Math.random() * posts.length )];
             console.log(randPost);
             const slackResponse = createResponse(randPost.data, body.user_id);
             
+            console.log(slackResponse);
+
+            postToSlack(slackResponse, body.channel_id);
+        })
+        .catch(error => {
+            const slackResponse = createError(error, body.user_id);
             console.log(slackResponse);
 
             postToSlack(slackResponse, body.channel_id);
